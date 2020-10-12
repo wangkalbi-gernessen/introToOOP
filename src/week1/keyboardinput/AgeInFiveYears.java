0b481c4d5253d3f7a99353fc95e71a4a8412a7fc
wangkalbi-gernessen <guizhou08chishui@gmail.com>
2020-10-12 01:04:42 -0700

    Complete AgeInFiveYears.java file

--- src/week1/keyboardinput/AgeInFiveYears.java
+++ src/week1/keyboardinput/AgeInFiveYears.java
@@ -4,7 +4,13 @@ import org.w3c.dom.ls.LSOutput;
 import java.util.Scanner;
 
 public class AgeInFiveYears {
-
-
-
+    public static void main(String[] args) {
+        Scanner obj = new Scanner(System.in);
+        System.out.println("Hello. What is your name?");
+        String name = obj.nextLine();
+        System.out.println("Hi," + name + "! How old are you?");
+        int age = obj.nextInt();
+        System.out.println("Did you know that in five years you will be " + (age + 5) + " years old?\n" +
+                "And five years ago you were " + (age - 5) + "! Imagine that!");
+    }
 }
