39a332f53b740221e064d6724fab5c83c1eb5e15
wangkalbi-gernessen <guizhou08chishui@gmail.com>
2020-10-12 01:10:46 -0700

    Complete AddingValuesInLoop.java file

--- src/week1/whileloops/AddingValuesInLoop.java
+++ src/week1/whileloops/AddingValuesInLoop.java
@@ -1,4 +1,19 @@
 package week1.whileloops;
+import java.util.Scanner;
 
 public class AddingValuesInLoop {
+    public static void main(String[] args) {
+        int sum = 0;
+        Scanner obj = new Scanner(System.in);
+        System.out.println("I will add up the numbers you give me");
+        System.out.println("Number:");
+        int num = obj.nextInt();
+        while(num != 0){
+            sum += num;
+            System.out.println("The total so far is " + sum);
+            System.out.println("Number:");
+            num = obj.nextInt();
+        }
+        System.out.println("The total is " + sum);
+    }
 }
