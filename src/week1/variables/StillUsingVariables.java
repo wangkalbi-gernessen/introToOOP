694770dd3b548fed43f85799ff55e7fb14fbb2b5
wangkalbi-gernessen <guizhou08chishui@gmail.com>
2020-10-12 00:56:03 -0700

    Complete StillUsingVariables.java file

--- src/week1/variables/StillUsingVariables.java
+++ src/week1/variables/StillUsingVariables.java
@@ -1,4 +1,9 @@
 package week1.variables;
 
 public class StillUsingVariables {
+    public static void main(String[] args) {
+        String name = "Kazunobu";
+        int year = 2022;
+        System.out.println("My name is " + name + " Valdez and I'll graduate in " + year + ".");
+    }
 }
