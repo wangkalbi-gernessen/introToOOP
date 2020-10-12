aab420ad7a0e8822036f14cc315355a6fa21d9af
wangkalbi-gernessen <guizhou08chishui@gmail.com>
2020-10-12 01:01:48 -0700

    Complete NameAgeSalary.js file

--- src/week1/keyboardinput/AgeInFiveYears.java
+++ src/week1/keyboardinput/AgeInFiveYears.java
@@ -1,4 +1,10 @@
 package week1.keyboardinput;
+import org.w3c.dom.ls.LSOutput;
+
+import java.util.Scanner;
 
 public class AgeInFiveYears {
+
+
+
 }
--- src/week1/keyboardinput/NameAgeSalary.java
+++ src/week1/keyboardinput/NameAgeSalary.java
@@ -1,4 +1,15 @@
 package week1.keyboardinput;
+import java.util.Scanner;
 
 public class NameAgeSalary {
+    public static void main(String[] args) {
+        Scanner obj = new Scanner(System.in);
+        System.out.println("Hello. What is your name?");
+        String name = obj.nextLine();
+        System.out.println("Hi," + name + "! How old are you?");
+        int age = obj.nextInt();
+        System.out.println("So you're " + age + ",eh? That's not old at all!\nHow much do you make," + name + "?");
+        double salary = obj.nextDouble();
+        System.out.println(salary + "! I hope that's per hour and not per year!LOL!");
+    }
 }
