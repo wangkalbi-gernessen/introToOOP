302e5cfef1af1894da01d3f8a8cdfb3d0791df51
wangkalbi-gernessen <guizhou08chishui@gmail.com>
2020-10-12 01:12:35 -0700

    Complete CountingMachine.java file

--- src/week1/forloops/CountingMachine.java
+++ src/week1/forloops/CountingMachine.java
@@ -1,4 +1,13 @@
 package week1.forloops;
+import java.util.Scanner;
 
 public class CountingMachine {
+    public static void main(String[] args) {
+        Scanner obj = new Scanner(System.in);
+        System.out.print("Count to:");
+        int num = obj.nextInt();
+        for(int i = 0; i <= num; i++){
+            System.out.print(i + " ");
+        }
+    }
 }
