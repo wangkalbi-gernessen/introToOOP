4b03cc5038d9bafc536075b7927cd64c24ff2411
wangkalbi-gernessen <guizhou08chishui@gmail.com>
2020-10-13 12:59:13 -0700

    Delete Valdez

--- src/week1/variables/StillUsingVariables.java
+++ src/week1/variables/StillUsingVariables.java
@@ -4,6 +4,6 @@ public class StillUsingVariables {
     public static void main(String[] args) {
         String name = "Kazunobu";
         int year = 2022;
-        System.out.println("My name is " + name + " Valdez and I'll graduate in " + year + ".");
+        System.out.println("My name is " + name + " and I'll graduate in " + year + ".");
     }
 }
